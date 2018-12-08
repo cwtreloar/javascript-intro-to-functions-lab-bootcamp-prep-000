ea72ac6811681d1958ea4b13c20232aaaba63716
Charles Treloar <cwtreloar@gmail.com>
2018-12-08 18:08:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  var lowercase = 'hello!'
+  var mixedCase = 'Hi there!'
+  if string = string.to UpperCase() === uppercase
+  
+}
